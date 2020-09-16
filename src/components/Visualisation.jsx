bb287753f617ea0e71046f08b5a3c94ffbb057bc
Edward John Steere <edward.steere@gmail.com>
2020-09-16 09:55:50 +0200

    Copy over example from force graph documentation

--- src/components/Visualisation.jsx
+++ src/components/Visualisation.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
-export const Visualisation = () => (
-  <div>Visualisation...</div>
+import { ForceGraph2D } from 'react-force-graph';
+
+
+export const Visualisation = ({ data }) => (
+  <ForceGraph2D
+    graphData={ data }
+    nodeAutoColorBy="group"
+    nodeCanvasObject={(node, ctx, globalScale) => {
+      const label = node.id;
+      const fontSize = 12/globalScale;
+      ctx.font = `${fontSize}px Sans-Serif`;
+      const textWidth = ctx.measureText(label).width;
+      const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+      ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillStyle = node.color;
+      ctx.fillText(label, node.x, node.y);
+    }}
+  />
 );
